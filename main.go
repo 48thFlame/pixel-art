81d570608c4b26b99b85beff127abc9a2cbac395
avitar64 <avishai@taragin.com>
2022-05-06 18:24:17 +0300

    fills red rect

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"syscall/js"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	document := js.Global().Get("document")
+	canvas := document.Call("getElementById", "canvas")
+
+	document.Get("body").Call("appendChild", canvas)
+	ctx := canvas.Call("getContext", "2d")
+	ctx.Set("fillStyle", "rgba(255, 0, 0, 1)")
+	width, height := canvas.Get("width"), canvas.Get("height")
+
+	ctx.Call("fillRect", 0, 0, width, height)
+
 }
